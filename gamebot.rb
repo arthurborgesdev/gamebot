a1ba4d3b42b381dad30fc3d2d532d3191de8e2c3
Arthur Borges <arthurborges.dev@gmail.com>
2021-04-06 12:37:29 -0300

    Add GuessGame first building blocks

--- gamebot.rb
+++ gamebot.rb
@@ -3,12 +3,30 @@ require 'json'
 require 'telegram/bot'
 
 Telegram::Bot::Client.run(ENV['BOT_TOKEN']) do |bot|
-  bot.listen do |message|
+  bot.listen do |message|     
     case message.text
     when '/start'
+      GuessGame.new()
+      
       bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    end
+    end  
   end
+end
+
+class GuessGame
+  def initialize()
+    @words = ["duck", "building", "plane", "girl", "refrigerator", "chair", "television", "bed"]
+  end
+
+  def greeting
+  end
+
+  def flush
+    
+  end
+
+  def random_word
+
 end
